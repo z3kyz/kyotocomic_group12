b11df8c349132fdbf4ee85a00155e66414ab709d
Dilushi Pabasara <z3ky.gdev.z@gmail.com>
2024-01-18 21:51:22 +0530

    update files

--- main.tf
+++ main.tf
@@ -7,23 +7,24 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "eu-north-1"
+  # Avoid hardcoding credentials directly; consider using environment variables or AWS CLI profiles.
+  # access_key = "AKIA47CRYKEONLQLQXXA"
+  # secret_key = "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"
+}
+
 resource "aws_ssm_parameter" "kyotocomic" {
   name  = "kyotocomic"
   type  = "String"
   value = "bar"
 }
 
-provider "aws" {
-  region = "eu-north-1"
-  access_key = "AKIA47CRYKEONLQLQXXA"
-  secret_key = "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"
-}
-
 resource "aws_s3_bucket" "bucket" {
   bucket = "kyotocomics3bucket123"
 
   tags = {
-    Name        = "My bucket"
+    Name = "My bucket"
   }
 }
 
@@ -39,16 +40,15 @@ resource "aws_key_pair" "key_pair" {
   public_key = tls_private_key.rsa_4096_kc.public_key_openssh
 }
 
-resource"local_file" "private_key" {
-  content = tls_private_key.rsa_4096_kc.private_key_pem
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096_kc.private_key_pem
   filename = var.key_name
 }
-
 
 resource "aws_instance" "public_instance" {
   ami           = "ami-0014ce3e52359afbd"
   instance_type = "t3.micro"
-  key_name = aws_key_pair.key_pair.key_name
+  key_name      = aws_key_pair.key_pair.key_name
 
   tags = {
     Name = "public_instance"
