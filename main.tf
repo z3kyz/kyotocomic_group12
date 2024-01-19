39175087b185d422cac88fd8eb4e517111f3dc08
Dilushi Pabasara <z3ky.gdev.z@gmail.com>
2024-01-19 09:53:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,7 +33,9 @@ resource "tls_private_key" "rsa_4096_kc" {
   rsa_bits  = 4096
 }
 
-variable "key_name" {}
+variable "key_name" {
+  default = "kyotocomic_pem"
+}
 
 resource "aws_key_pair" "key_pair" {
   key_name   = var.key_name
